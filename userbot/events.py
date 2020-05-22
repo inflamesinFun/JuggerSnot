70ffacfe1f2ae4044e9ccf562dafa1b5cfce8136
XNulI <65774707+XNulI@users.noreply.github.com>
2020-05-23 01:35:28 +0300

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -97,7 +97,7 @@ def register(**args):
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     text = "**USERBOT HATA RAPORU**\n"
-                    link = "[Asena Destek Grubu](https://t.me/AsenaSupport)"
+                    link = "[Turhan Destek Grubu](https://t.me/turhanuserbotsupport)"
                     text += "İsterseniz, bunu rapor edebilirsiniz"
                     text += f"- sadece bu mesajı buraya iletin {link}.\n"
                     text += "Hata ve Tarih dışında hiçbir şey kaydedilmez\n"
