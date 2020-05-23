a2d5a6b45af5aa1852db845b62a09d3e58f0d803
xNull <65774707+XNulI@users.noreply.github.com>
2020-05-24 02:07:04 +0300

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -728,6 +728,35 @@ async def fuk(event):
         await asyncio.sleep(animation_interval)
         await event.edit(animation_chars[i % 4])
 
+                      
+@register(outgoing=True, pattern="^.otuzbir")
+async def otuzbir(event):
+    if event.fwd_from:
+        return
+                      
+    animation_interval = 0.3
+    animation_ttl = range(0, 12)
+    await event.edit("31 Çekiorm Lna")
+    animation_chars = [
+            "8=👊====D",
+            "8==👊===D",
+            "8===👊==D",
+            "8====👊=D",
+            "8=====👊D",    
+            "8====👊=D",
+            "8===👊==D",
+            "8==👊===D",
+            "8=👊====D",
+            "8👊=====D",
+            "Ohh",
+            "💦"
+ ]
+
+    for i in animation_ttl:
+
+        await asyncio.sleep(animation_interval)
+
+        await event.edit(animation_chars[i % 12])
 
 @register(outgoing=True, pattern="^.kalp (.*)")
 async def _(event):
